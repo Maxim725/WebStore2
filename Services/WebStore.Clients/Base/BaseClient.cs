19d70ebff6e71ce20b077fecc910b6f87107e4f7
maksim <maks_10000@bk.ru>
2020-09-20 10:33:06 +0300

    Базовый клиент для WebApi

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly string _serviceAddress;
+        protected readonly HttpClient _client;
+
+        protected BaseClient(IConfiguration configuration, string ServiceAddress)
+        {
+            _serviceAddress = ServiceAddress;
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(configuration["WebApiUrl"]),
+                DefaultRequestHeaders =
+                {
+                    Accept = { new MediaTypeWithQualityHeaderValue("application/json") }
+                }
+            };
+        }
+    }
+}
