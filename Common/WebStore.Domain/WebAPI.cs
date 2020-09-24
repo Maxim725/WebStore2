16441c613c8d66258bcf2ff8b8b4b6a1acd4eef1
maksim <maks_10000@bk.ru>
2020-09-24 15:55:14 +0300

    Баги и добавление OrderDTO

--- Common/WebStore.Domain/WebAPI.cs
+++ Common/WebStore.Domain/WebAPI.cs
@@ -9,5 +9,6 @@ namespace WebStore.Domain
     {
         public const string Employees = "api/employees";
         public const string Products = "api/products";
+        public const string Orders = "api/orders";
     }
 }
