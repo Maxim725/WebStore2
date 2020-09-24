cf821ded2d84b7646044b23999be6d71193f93a2
maksim <maks_10000@bk.ru>
2020-09-24 14:24:54 +0300

    Добавление проекции данных для Brand Product Section

--- Services/WebStore.Services/Mapping/ProductMapper.cs
+++ Services/WebStore.Services/Mapping/ProductMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using WebStore.Domain.DTO.Products;
 using WebStore.Domain.Entities;
 using WebStore.Domain.ViewModels;
 
@@ -18,5 +19,28 @@ namespace WebStore.Services.Mapping
         };
 
         public static IEnumerable<ProductViewModel> ToView(this IEnumerable<Product> products) => products.Select(ToView);
+
+
+        public static ProductDTO ToDTO(this Product product) => product is null ? null : new ProductDTO
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Order = product.Order,
+            Price = product.Price,
+            ImageUrl = product.ImageUrl,
+            Brand = product.Brand.ToDTO(),
+            Section = product.Section.ToDTO()
+        };
+
+        public static Product FromDTO(this ProductDTO product) => product is null ? null : new Product
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Order = product.Order,
+            Price = product.Price,
+            ImageUrl = product.ImageUrl,
+            Brand = product.Brand.FromDTO(),
+            Section = product.Section.FromDTO()
+        };
     }
 }
