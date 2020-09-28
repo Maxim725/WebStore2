2d91547e7130e9a35a507becca7a2ef76c3b10c6
maksim <maks_10000@bk.ru>
2020-09-28 10:35:06 +0300

    Доработка асинхронных операций

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebStore.Clients.Base
@@ -28,36 +29,45 @@ namespace WebStore.Clients.Base
 
         protected T Get<T>(string url) => GetAsync<T>(url).Result;
 
-        protected async Task<T> GetAsync<T>(string url)
+        protected async Task<T> GetAsync<T>(string url, CancellationToken cancel = default)
         {
-            var response = await _client.GetAsync(url);
+            var response = await _client.GetAsync(url, cancel);
 
             return await response
                 .EnsureSuccessStatusCode() // Убеждаемя, что в ответе 200-й код иначе ошибка
                 .Content // В ответе есть содержимое с которым можно работать
-                .ReadAsAsync<T>(); // десереализация объекта
+                .ReadAsAsync<T>(cancel); // десереализация объекта
 
         }
 
         protected HttpResponseMessage Post<T>(string url, T item) => PostAsync<T>(url, item).Result;
-        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item, CancellationToken cancel = default)
         {
-            var response = await _client.PostAsJsonAsync(url, item);
+            var response = await _client.PostAsJsonAsync(url, item, cancel);
             return response.EnsureSuccessStatusCode();
         }
 
 
         protected HttpResponseMessage Put<T>(string url, T item) => PutAsync<T>(url, item).Result;
-        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item, CancellationToken cancel = default)
         {
-            var response = await _client.PutAsJsonAsync(url, item);
+            var response = await _client.PutAsJsonAsync(url, item, cancel);
             return response.EnsureSuccessStatusCode();
         }
 
         protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
-        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken cancel = default)
         {
-            var response = await _client.DeleteAsync(url);
+            var response = await _client.DeleteAsync(url, cancel);
+
+            if (cancel.IsCancellationRequested)
+            {
+                // непонятные опреации
+                // шото убрать, шото логировать
+            }
+            // Нужно периодически вызывать этот метод, если через токен пришла отмена операции, 
+            // то выбрасывается исключение
+            cancel.ThrowIfCancellationRequested();
             return response;
         }
 
