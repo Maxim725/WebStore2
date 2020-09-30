82cf367e1e8038cff46dead8af41eaf4c6f97c68
maksim <maks_10000@bk.ru>
2020-09-30 18:54:25 +0300

    Добавление областей

--- UI/WebStore/Controllers/AccountController.cs
+++ UI/WebStore/Controllers/AccountController.cs
@@ -31,35 +31,38 @@ namespace WebStore.Controllers
         {
             if (!ModelState.IsValid) return View(Model);
 
-            _logger.LogInformation("Начало процесса регистрации нового пользователя {0}", Model.UserName);
-            var user = new User
+           using(_logger.BeginScope("Регистрация пользователя {0}", Model.UserName))
             {
-                UserName = Model.UserName
-            };
+                _logger.LogInformation("Начало процесса регистрации нового пользователя {0}", Model.UserName);
+                var user = new User
+                {
+                    UserName = Model.UserName
+                };
 
-            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
-            if (registration_result.Succeeded)
-            {
-                _logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
-                await _UserManager.AddToRoleAsync(user, Role.User);
+                var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+                if (registration_result.Succeeded)
+                {
+                    _logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
+                    await _UserManager.AddToRoleAsync(user, Role.User);
 
-                _logger.LogInformation("Пользователь {0} наделён ролью {1}", user.UserName, Role.User);
+                    _logger.LogInformation("Пользователь {0} наделён ролью {1}", user.UserName, Role.User);
 
-                await _SignInManager.SignInAsync(user, false);
+                    await _SignInManager.SignInAsync(user, false);
 
-                _logger.LogInformation("Пользователь {0} автоматически вошёл в систему регистрации", user.UserName, Role.User);
+                    _logger.LogInformation("Пользователь {0} автоматически вошёл в систему регистрации", user.UserName, Role.User);
 
-                return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home");
+                }
+
+                //_logger.Log(LogLevel.Information, new EventId(4), "gg", null); // более общий метод логирования)
+                _logger.LogWarning("Ошибка при регистрации нового пользователя {0}\r\n{1}",
+                            Model.UserName,
+                            string.Join(Environment.NewLine, registration_result.Errors.Select(e => e.Description)));
+
+                foreach (var error in registration_result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+
             }
-
-            //_logger.Log(LogLevel.Information, new EventId(4), "gg", null); // более общий метод логирования)
-            _logger.LogWarning("Ошибка при регистрации нового пользователя {0}\r\n{1}",
-                        Model.UserName,
-                        string.Join(Environment.NewLine, registration_result.Errors.Select(e => e.Description)));
-
-            foreach (var error in registration_result.Errors)
-                ModelState.AddModelError(string.Empty, error.Description);
-
             return View(Model);
         }
 
--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WebStore
 {
@@ -12,6 +13,15 @@ namespace WebStore
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(host => host
-                   .UseStartup<Startup>());
+                   .UseStartup<Startup>()
+                .ConfigureLogging((host, log) =>
+                {
+                    // log.ClearProviders() // удаление провайдеров конфигураций
+                    // log.AddConsole(); // консольный провайдер
+                    // log.AddConsole(opt => opt.IncludeScopes = true); // консольный провайдер
+                    // log.AddProvider();
+                    // log.AddFilter(level => level >= LogLevel.Information); // Фильтр по типам ошибки
+                    // log.AddFilter("Microsoft", level => level >= LogLevel.Information); // По категориям
+                }));
     }
 }
