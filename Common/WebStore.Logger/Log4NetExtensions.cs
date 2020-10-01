46d8cbdb1dfaf36934a2957e5925a3630c0422de
maksim <maks_10000@bk.ru>
2020-10-01 11:03:39 +0300

    Логгер на основе библиотеки log4net

---
+++ Common/WebStore.Logger/Log4NetExtensions.cs
@@ -0,0 +1,29 @@
+﻿using log4net.Repository.Hierarchy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Xml.Schema;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetExtensions
+    {
+        public static Microsoft.Extensions.Logging.ILoggerFactory AddLog4Net(this Microsoft.Extensions.Logging.ILoggerFactory factory, 
+            string configuration = "log4net.config")
+        {
+            if (Path.IsPathRooted(configuration))
+            {
+                var assembly = Assembly.GetEntryAssembly() ??
+                    throw new InvalidOperationException("Не улаось определить сборку");
+                var dir = Path.GetDirectoryName(assembly.Location) ??
+                    throw new InvalidOperationException("Не удалось определить папку со сборкой");
+
+                configuration = Path.Combine(dir, configuration);
+            }
+            factory.AddProvider(new Log4NetLoggerProvider(configuration));
+            return factory;
+        }
+    }
+}
---
+++ Common/WebStore.Logger/Log4NetLogger.cs
@@ -0,0 +1,99 @@
+﻿using log4net;
+using log4net.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Reflection;
+using System.Xml;
+
+namespace WebStore.Logger
+{
+    public class Log4NetLogger : ILogger
+    {
+        private readonly ILog _log;
+        public Log4NetLogger(string category, XmlElement xmlConfiguration)
+        {
+            var loggerRepository = LogManager.CreateRepository(
+                Assembly.GetEntryAssembly(),
+                typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            _log = LogManager.GetLogger(loggerRepository.Name, category);
+
+            log4net.Config.XmlConfigurator.Configure(loggerRepository, xmlConfiguration);
+
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            // Не поддерживает области журнала
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
+                case LogLevel.None:
+                    return false;
+
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    return _log.IsDebugEnabled;
+
+                case LogLevel.Information:
+                    return _log.IsInfoEnabled;
+
+                case LogLevel.Warning:
+                    return _log.IsWarnEnabled;
+
+                case LogLevel.Error:
+                    return _log.IsErrorEnabled;
+
+                case LogLevel.Critical:
+                    return _log.IsFatalEnabled;
+
+
+            }
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, 
+                TState state, Exception exception, 
+                Func<TState, Exception, string> formatter)
+        {
+            if (formatter is null)
+                throw new ArgumentNullException(nameof(formatter));
+
+            if (!IsEnabled(logLevel)) return;
+
+            var logMessage = formatter(state, exception);
+
+            if (string.IsNullOrEmpty(logMessage) && exception is null) return;
+
+            switch (logLevel)
+            {
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
+                case LogLevel.None:
+                    break;
+                
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    _log.Debug(logMessage);
+                    break;
+                case LogLevel.Information:
+                    _log.Info(logMessage);
+                    break;
+                case LogLevel.Warning:
+                    _log.Warn(logMessage);
+                    break;
+                case LogLevel.Error:
+                    _log.Error(logMessage);
+                    break;
+                case LogLevel.Critical:
+                    _log.Fatal(logMessage);
+                    break;
+            }
+        }
+    }
+}
