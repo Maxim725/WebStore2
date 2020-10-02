90d06e0b1131c906ccad5e1866a1107fa5ae854e
maksim <maks_10000@bk.ru>
2020-10-02 15:38:54 +0300

    Первый модульный тест

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Areas.Admin.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void IndexReturnsView()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
