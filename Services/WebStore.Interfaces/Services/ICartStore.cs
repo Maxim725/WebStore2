a85186977abb38d041cc28aa48e6634a0f17f64d
maksim <maks_10000@bk.ru>
2020-10-03 14:45:34 +0300

    Сервис хранилища данных корзины

---
+++ Services/WebStore.Interfaces/Services/ICartStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface ICartStore
+    {
+        Cart Cart { get; set; }
+    }
+}
