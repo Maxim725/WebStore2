d8122bc3c9239c31d1474f6be6773091c88eb793
maksim <maks_10000@bk.ru>
2020-10-08 15:17:52 +0300

    Подготовка визуального компонента

---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    { 
+        private readonly IProductData _productData;
+
+        public BreadCrumbsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadCrumbViewModel();
+
+            // Извлечение данных модели и по секции по их идентификаторам
+            if(int.TryParse(ViewContext.RouteData.Values["id"]?.ToString(), out var productId))
+            {
+                var product = _productData.GetProductById(productId);
+
+                if (product != null)
+                    model.Product = product.Name;
+            }
+            return View();
+        }
+    }
+}
