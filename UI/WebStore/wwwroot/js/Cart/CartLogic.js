8dd50002273cc3b447392b97d569dbded0fd102f
maksim <maks_10000@bk.ru>
2020-10-12 10:48:22 +0300

    добавлен скрипт в корзину

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -0,0 +1,50 @@
+﻿Cart = {
+
+	_properties: {
+		getCartViewLink: "",
+		addToCartLink: ""
+	},
+
+	init: function (properties) {
+		$.extend(Cart._properties.properties);
+
+		$(".add-to-cart").click(addToCart);
+
+	},
+
+	addToCart: function (event) {
+		event.preventDefault();
+
+		var button = $(this)
+
+		const id = buttoin.data("id") // data-id
+
+		$.get(Cart._properties.addToCartLink + "/" + id)
+			.done(function () {
+				Cart.showToolTip(button);
+				Cart.refreshCartView();
+			})
+			.fail(function () {
+				console.log("addToCart fail");
+			});
+	},
+
+	showToolTip: function (button) {
+		button.tooltip({ title: "Добавлено в корзину" }).showToolTip("show");
+		setTimeout(function () {
+			button.tooltip("destroy");
+		}, 500);
+	},
+
+	refreshCartView: function () {
+		var container = $("#cart-container");
+		$.get(Cart._properties.getCartViewLink)
+			.done(function (carthtml) {
+				container.html(cartHtml);
+			})
+			.fail(function () {
+				console.log("refreshCartView fail");
+			});
+	}
+
+}
