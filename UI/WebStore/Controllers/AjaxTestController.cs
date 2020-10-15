3d51b206fb6242d60e12a230030e464280edbb0c
maksim <maks_10000@bk.ru>
2020-10-15 11:47:06 +0300

    примеры использования ajax

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModel;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> GetJSON(int? id, string msg)
+        {
+            await Task.Delay(2000);
+
+            return Json(new {
+                Message = $"Response (id:{id ?? -1}): {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            });
+        }
+
+        public async Task<IActionResult> GetTestView(int? id, string msg)
+        {
+            await Task.Delay(2000);
+
+            return PartialView("Partial/_DataView", new AjaxTestViewModel
+            {
+                Id = id,
+                Message = msg,
+            });
+        }
+    }
+}
---
+++ UI/WebStore/ViewModel/AjaxTestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebStore.ViewModel
+{
+    public class AjaxTestViewModel
+    {
+        public int? Id { get; set; }
+        public string Message { get; set; }
+
+        public DateTime ServerTime { get; set; } = DateTime.Now;
+    }
+}
