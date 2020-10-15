4cab161144fae8de623a8691b29012595f51fe6e
maksim <maks_10000@bk.ru>
2020-10-15 10:59:51 +0300

    Валидачия на стороне клиента

--- Common/WebStore.Domain/ViewModels/Identity/RegisterUserViewModel.cs
+++ Common/WebStore.Domain/ViewModels/Identity/RegisterUserViewModel.cs
@@ -1,22 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebStore.Domain.ViewModels.Identity
 {
     public class RegisterUserViewModel
     {
-        [Required, MaxLength(256)]
+        [Required(ErrorMessage = "Обязательное поле")]
+        [MaxLength(256, ErrorMessage = "Длина строки должна быть меньше 256 символов")]
+        [MinLength(4, ErrorMessage = "Длина строки должна быть больше 4 символов")]
         [Display(Name = "Имя пользователя")]
+        [Remote("IsNameFree", "Account")]
         public string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Обязательное поле")]
         [Display(Name = "Пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Обязательное поле")]
         [Display(Name = "Подтверждение пароля")]
         [DataType(DataType.Password)]
-        [Compare(nameof(Password))]
+        [Compare(nameof(Password), ErrorMessage = "Пароли должны совпадать")]
         public string PasswordConfirm { get; set; }
     }
 }
